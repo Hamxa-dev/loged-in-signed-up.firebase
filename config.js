804d206d270f762d573319765258189da6610f27
Hafiz Hamza <hafizhamxa8@gmail.com>
2023-11-16 13:02:37 +0500

    first commend

--- config.js
+++ config.js
@@ -1,19 +1,21 @@
 
- 
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
-  import { getAuth } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
 
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyBbYOiRTKtFC4deHC-khGs7yY1NyX5VjAs",
-    authDomain: "sign-up-form-7d566.firebaseapp.com",
-    projectId: "sign-up-form-7d566",
-    storageBucket: "sign-up-form-7d566.appspot.com",
-    messagingSenderId: "812671101792",
-    appId: "1:812671101792:web:e358804662302e566fcc05",
-    measurementId: "G-MENK4SQ6N0"
-  };
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
+import { getFirestore, collection, addDoc, getDocs, query, where } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-firestore.js";
 
- export const app = initializeApp(firebaseConfig);
-  export const auth = getAuth(app);
- 
+const firebaseConfig = {
+  apiKey: "AIzaSyBbYOiRTKtFC4deHC-khGs7yY1NyX5VjAs",
+  authDomain: "sign-up-form-7d566.firebaseapp.com",
+  projectId: "sign-up-form-7d566",
+  storageBucket: "sign-up-form-7d566.appspot.com",
+  messagingSenderId: "812671101792",
+  appId: "1:812671101792:web:e358804662302e566fcc05",
+  measurementId: "G-MENK4SQ6N0",
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export { collection, addDoc, getDocs, query, where  };
--- register.js
+++ register.js
@@ -1,35 +1,60 @@
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
-import { auth } from "./config.js";
+import {collection,addDoc,} from "https://www.gstatic.com/firebasejs/10.5.2/firebase-firestore.js"; 
+import { auth, db } from "./config.js";
+
 
 const email = document.querySelector("#signupUserEmail");
 const password = document.querySelector("#signupPassword");
 const form = document.querySelector(".form");
+const name = document.querySelector(".name");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   createUserWithEmailAndPassword(auth, email.value, password.value)
     .then((userCredential) => {
       const user = userCredential.user;
+
       console.log(user);
+
+      // Add the user data to Firestoref
+      return addDoc(collection(db, "user"), {
+        name: name.value,
+        email: email.value,
+        uid: user.uid,
+      });
+    })
+    .then(() => {
+      // Display success message
       Swal.fire({
         position: "top-center",
         icon: "success",
-        title: "Your successfully signed up",
+        title: "You're successfully signed up",
         showConfirmButton: false,
         timer: 1500,
       });
+
+      // Clear input fields
       password.value = "";
       email.value = "";
+      name.value = "";
+
+      // Delay the redirection to the login page for 1 second
+      setTimeout(() => {
+        window.location = "index(login).html";
+      }, 1000);
     })
     .catch((error) => {
-      const errorCode = error.code;
+     
       const errorMessage = error.message;
+
+      // Display error message
       Swal.fire({
         icon: "error",
         title: "Oops...",
-        text: "Email alredy in use",
+        text: "Email already in use",
         footer: '<a href="">Why do I have this issue?</a>',
       });
-      console.log(errorMessage);
+
+      console.error(errorMessage);
     });
 });
